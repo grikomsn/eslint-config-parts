cd5f85040b56d8843102aec5506dd85b4370e11d
Griko Nibras <git@griko.id>
2021-06-28 08:07:32 +0700

    refactor(eslint): update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
-module.exports = {
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const fs = require('fs');
+const path = require('path');
+
+// https://github.com/kentcdodds/eslint-config-kentcdodds/blob/28c760375858c2c2b02ea2561dae0de465596350/non-rules-config.js#L4-L8
+// eslint-disable-next-line no-nested-ternary
+const tsConfig = fs.existsSync('tsconfig.json')
+  ? path.resolve('tsconfig.json')
+  : fs.existsSync('types/tsconfig.json')
+  ? path.resolve('types/tsconfig.json')
+  : undefined;
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
   extends: [
     'airbnb-typescript',
     'airbnb/hooks',
@@ -13,7 +28,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: tsConfig,
   },
   plugins: ['@typescript-eslint', 'import', 'prettier', 'react'],
 
@@ -24,3 +39,5 @@ module.exports = {
     // add your own!
   },
 };
+
+module.exports = config;
